8db35ab0f23bfd475ca7757c0eebe26b41410ae3
Gimblet <gimbletdev@gmail.com>
2026-02-28 00:01:47 -0500

    feat: prepared por cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   vite: {
     resolve: {
       alias: {
@@ -15,16 +18,22 @@ export default defineConfig({
       },
     },
   },
+
   output: "static",
+
   build: {
     inlineStylesheets: "auto",
   },
+
   server: {
     host: true,
     port: 4321,
   },
+
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "es",
-  }
+  },
+
+  adapter: cloudflare()
 });
